e763e2d2527e1fa815875b66cdbf503af3329880
kamruzzaman1351 <mdrashed1351@gmail.com>
2022-06-10 23:44:57 +0600

    Creating the Forgot password section

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
-
+import {useState} from 'react'
+import { ArrowRightIcon } from '../assets'
+import { Link, useNavigate  } from 'react-router-dom'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
+import { toast } from 'react-toastify'
 const ForgotPassword = () => {
+  const navigate = useNavigate()
+  const [email, setEmail] = useState('')
+
+  const onChange = e => {
+      setEmail(e.target.value)
+  }
+  const onSubmit = async(e) => {
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      await sendPasswordResetEmail(auth, email)
+      toast.success("Email Send", {autoClose:1000})
+      navigate("/login")
+    } catch (error) {
+      toast.error("Something went wrong!", {autoClose:2000})
+    }
+  }
+
   return (
-    <div>ForgotPassword</div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Forgot Password</p>
+        </header>
+        <main>
+          <form onSubmit={onSubmit}>
+            <input 
+              type="email"
+              id="email"
+              className="emailInput"
+              placeholder="Email"
+              value={email}
+              onChange={onChange}
+            />
+            <Link 
+              to="/login"
+              className="forgotPasswordLink" 
+            >
+              Sign In
+            </Link> 
+            <div className="signInBar">
+              <p className="signInText">
+                Send Email
+              </p>
+              <button className="signInButton">
+                <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+              </button>
+            </div>
+          </form>
+        </main>
+      </div>
+    </>
   )
 }
 
